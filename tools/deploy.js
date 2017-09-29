cb2ca975859c6a5b7aba9df87f9316a1954cbf4a
David Lemvigh <dlemvigh@gmail.com>
2017-09-29 23:48:15 +0200

    github pages config

--- tools/deploy.js
+++ tools/deploy.js
@@ -16,9 +16,9 @@ import run from './run';
 // GitHub Pages
 const remote = {
   name: 'github',
-  url: 'https://github.com/<user>/<repo>.git',
+  url: 'https://github.com/dlemvigh/ironman-react-starter-kit.git',
   branch: 'gh-pages',
-  website: 'https://<user>.github.io/<repo>/',
+  website: 'https://dlemvigh.github.io/ironman-react-starter-kit/',
   static: true,
 };
 
